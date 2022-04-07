3ab76f1752174ab0231cb0f8a686553c7deb95e1
diego <diego@route.com>
2022-04-06 22:18:15 -0300

    feat: adding the files for the lesson 01

---
+++ domain/model/bank.go
@@ -0,0 +1,48 @@
+package model
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type Bank struct {
+	Base     `valid:"required"`
+	Name     string     `json:"name" valid:"notnull"`
+	Code     string     `json:"code" valid:"notnull"`
+	Accounts []*Account `valid:"-"`
+}
+
+// Isso é um método no Go.
+func (bank *Bank) isValid() error {
+	_, err := govalidator.ValidateStruct(bank)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Isso é uma função no Go.
+func NewBank(name string, code string) (*Bank, error) {
+	bank := Bank{
+		Name: name,
+		Code: code,
+	}
+
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
+	bank.UpdatedAt = time.Now()
+
+	// Retorna o error na variável de `err`
+	err := bank.isValid()
+	// Se houver algum error na camada de Model, então retorna algo.
+	if err != nil {
+		return nil, err
+	}
+
+	// Por ser um ponteiro, é necessário retornar a referência em memória e não o objeto em si.
+	return &bank, nil
+}
